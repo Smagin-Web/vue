666734b9fac0048361bdc840a5a5759c3d2a00c0
smaginweb <n.smagin@vk.team>
2023-10-03 18:13:37 +0300

    [FEATURE] Add hover effect in cards masters

--- src/components/cards/CardMaster.vue
+++ src/components/cards/CardMaster.vue
@@ -5,7 +5,10 @@ import IconPlay from '../icons/IconPlay.vue'
 <template>
 	<div class="card">
 		<IconPlay class="card-icon" />
-		<img src="./Avatar.png" class="card-img" />
+
+		<div class="card-img-wrapper">
+			<img src="./Avatar.png" class="card-img" />
+		</div>
 
 		<h5 class="card-heading">Ольга Заика</h5>
 		<p class="card-text">Главный врач, врач-косметолог</p>
@@ -29,10 +32,6 @@ import IconPlay from '../icons/IconPlay.vue'
 	transition: 0.2s;
 }
 
-.card:hover .card-img {
-	transform: scale(0.98);
-}
-
 .card:hover .card-icon {
 	opacity: 1;
 	z-index: 20;
@@ -41,13 +40,37 @@ import IconPlay from '../icons/IconPlay.vue'
 .card-img {
 	width: 100%;
 	height: auto;
-	margin-bottom: 40px;
+	transition: 0.2s;
+}
+
+.card-img-wrapper:after {
+	content: '';
+	display: block;
+	position: absolute;
+	left: -3px;
+	right: -3px;
+	bottom: -3px;
+	top: -3px;
+	border-radius: 50%;
+	transition: 0.2s ease;
+	border: 1px solid #f4eee8;
+}
+
+.card:hover .card-img-wrapper:after {
+	border: 10px solid #f4eee8;
+}
+
+.card-img-wrapper {
+	position: relative;
+	width: 100%;
+	height: auto;
 	transition: 0.2s;
 }
 
 .card-heading {
 	text-align: center;
 	font-size: 30px;
+	padding-top: 38px;
 	padding-bottom: 22px;
 }
 
