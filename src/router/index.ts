594f38d54d9c0bb4b9e009ddf0c306d5e7646441
smaginweb <n.smagin@vk.team>
2023-11-18 17:14:33 +0300

    [FIX] Fix Router.ts file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,9 @@
-import { createRouter, createWebHistory, createMemoryHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import InsideView from '../views/InsideView.vue'
 
-const baseUrl = import.meta.env.BASE_URL
-const history = import.meta.env.SSR
-	? createMemoryHistory(baseUrl)
-	: createWebHistory(baseUrl)
-
 const router = createRouter({
-	history,
+	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: '/',
