28d12af1835fd5f58150e9681cc498cef8d54b65
smaginweb <n.smagin@vk.team>
2023-11-18 17:08:06 +0300

    [FEATURE] Add SSR

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createMemoryHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import InsideView from '../views/InsideView.vue'
 
+const baseUrl = import.meta.env.BASE_URL
+const history = import.meta.env.SSR
+	? createMemoryHistory(baseUrl)
+	: createWebHistory(baseUrl)
+
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history,
 	routes: [
 		{
 			path: '/',
