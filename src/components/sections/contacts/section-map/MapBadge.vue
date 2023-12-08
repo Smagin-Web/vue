a6614b819ec982db62a85ec547f8e12be60f74ad
smaginweb <n.smagin@vk.team>
2023-12-08 15:03:49 +0300

    [FEATURE] Add map badges

---
+++ src/components/sections/contacts/section-map/MapBadge.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const props = defineProps(['number', 'color'])
+
+const styleColor = `
+background-color: ${props.color || '#ffe116'};
+color: ${props.color ? '#fff' : '#000'}
+`
+</script>
+
+<template>
+	<div class="map-badge">
+		<div class="map-badge-number" :style="styleColor">{{ number }}</div>
+		<p class="text-sm"><slot /></p>
+	</div>
+</template>
+
+<style scoped>
+.map-badge {
+	display: flex;
+	align-items: center;
+	gap: 10px;
+}
+
+.map-badge-number {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 31px;
+	height: 31px;
+	border-radius: 50%;
+
+	color: #000;
+	font-family: 'BrisaSans';
+	font-size: 22px;
+	font-weight: 700;
+}
+</style>
--- src/components/sections/contacts/section-map/SectionMap.vue
+++ src/components/sections/contacts/section-map/SectionMap.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import MContainer from '@/components/shared/MContainer.vue'
+import MapBadge from './MapBadge.vue'
 </script>
 
 <template>
@@ -9,14 +10,19 @@ import MContainer from '@/components/shared/MContainer.vue'
 			<div class="map">
 				<div class="map-header">
 					<h6 class="map-header-title">Ближайшие станции метро</h6>
-					<div style="padding-bottom: 32px">Бэйджики</div>
+					<div class="map-header-badges">
+						<MapBadge number="8">Авиамоторная</MapBadge>
+						<MapBadge number="8">Площадь Ильича</MapBadge>
+						<MapBadge number="11" color="#7ECECE">Лефортово</MapBadge>
+						<MapBadge number="10" color="#BED968">Римская</MapBadge>
+					</div>
 				</div>
 				<div class="map-body">
 					<iframe
 						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2245.4343035283673!2d37.61500057720295!3d55.75095739213286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54a50b315e573%3A0xa886bf5a3d9b2e68!2z0JzQvtGB0LrQvtCy0YHQutC40Lkg0JrRgNC10LzQu9GM!5e0!3m2!1sru!2sru!4v1702030838589!5m2!1sru!2sru"
 						width="600"
 						height="450"
-						style="border: 0; width: 100%; border-radius: 40px;"
+						style="border: 0; width: 100%; border-radius: 34px"
 						loading="lazy"
 						referrerpolicy="no-referrer-when-downgrade"
 					></iframe>
@@ -41,6 +47,13 @@ import MContainer from '@/components/shared/MContainer.vue'
 .map-header {
 	padding-left: 40px;
 	padding-right: 40px;
+}
+
+.map-header-badges {
+  padding-top: 32px;
+	padding-bottom: 32px;
+	display: flex;
+	gap: 30px;
 }
 
 .map-body {
