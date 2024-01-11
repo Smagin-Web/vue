c64a5c6e2768e8dcb5c493e02bb9dd56f9ca60c5
smaginweb <n.smagin@vk.team>
2024-01-11 17:18:30 +0300

    [FIX] Faq Item

--- src/components/sections/faq/SectionFaq.vue
+++ src/components/sections/faq/SectionFaq.vue
@@ -3,6 +3,41 @@ import MContainer from '@/components/shared/MContainer.vue'
 import BreadCrumbs from '@/components/shared/BreadCrumbs.vue'
 import Filter from '@/components/shared/Filter.vue'
 import FaqItem from '../inside/section-faq/FaqItem.vue'
+import { ref } from 'vue'
+
+let indexActiveItem = ref('0')
+
+const increment1 = () => {
+	if (indexActiveItem.value === '1') {
+		indexActiveItem.value = '0'
+	} else {
+		indexActiveItem.value = '1'
+	}
+}
+
+const increment2 = () => {
+	if (indexActiveItem.value === '2') {
+		indexActiveItem.value = '0'
+	} else {
+		indexActiveItem.value = '2'
+	}
+}
+
+const increment3 = () => {
+	if (indexActiveItem.value === '3') {
+		indexActiveItem.value = '0'
+	} else {
+		indexActiveItem.value = '3'
+	}
+}
+
+const increment4 = () => {
+	if (indexActiveItem.value === '4') {
+		indexActiveItem.value = '0'
+	} else {
+		indexActiveItem.value = '4'
+	}
+}
 </script>
 
 <template>
@@ -24,18 +59,26 @@ import FaqItem from '../inside/section-faq/FaqItem.vue'
 			</div>
 			<div class="questions">
 				<FaqItem
+					:onClick="() => increment1()"
+					:isActive="indexActiveItem === '1'"
 					title="Посоветуйте пожалуйста процедуру для ухода и омоложения кожи вокруг глаз? Не хочу пока прибегать к инъекционным методикам."
 					text="text"
 				/>
 				<FaqItem
+					:onClick="() => increment2()"
+					:isActive="indexActiveItem === '2'"
 					title="Хочу поставить ботокс в лоб. Сколько уколов потребуется?"
 					text="text"
 				/>
 				<FaqItem
+					:onClick="() => increment3()"
+					:isActive="indexActiveItem === '3'"
 					title="Можно ли вылечить вросший ноготь навсегда? У меня ноготь врастает уже на протяжении полутора лет."
 					text="text"
 				/>
 				<FaqItem
+					:onClick="() => increment4()"
+					:isActive="indexActiveItem === '4'"
 					title="На теле есть несколько висячих родинок. Скажите пожалуйста, их нужно удалять или, если они не беспокоят, можно оставить? Сколько стоит удаление в клинике? "
 					text="text"
 				/>
