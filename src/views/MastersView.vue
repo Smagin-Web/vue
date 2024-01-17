5aaa339798b08557e75a5eadec4a8ee8c9cd6396
smaginweb <n.smagin@vk.team>
2024-01-17 08:43:09 +0300

    [FIX] Page MastersView - delete section help

--- src/views/MastersView.vue
+++ src/views/MastersView.vue
@@ -2,12 +2,10 @@
 import ViewWrapper from '@/components/ViewWrapper.vue'
 
 import SectionMasters from '@/components/sections/masters/SectionMasters.vue'
-import SectionHelp from '@/components/sections/shared/section-help/SectionHelp.vue'
 </script>
 
 <template>
 	<ViewWrapper>
 		<SectionMasters />
-		<SectionHelp />
 	</ViewWrapper>
 </template>
