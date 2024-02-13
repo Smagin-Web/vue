5827b927ce894a32c611124188feeac03ca8ba26
smaginweb <n.smagin@vk.team>
2024-02-13 05:29:49 +0300

    [FIX] Small fix

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -2,13 +2,13 @@
 import ViewWrapper from '@/components/ViewWrapper.vue'
 import SectionContactsCard from '@/components/sections/contacts/section-contacts-card/SectionContactsCard.vue'
 import SectionMap from '@/components/sections/contacts/section-map/SectionMap.vue'
-import SectionTour from '@/components/sections/about/section-tour/SectionTour.vue'
+// import SectionTour from '@/components/sections/about/section-tour/SectionTour.vue'
 </script>
 
 <template>
 	<ViewWrapper>
 		<SectionContactsCard />
     <SectionMap />
-		<SectionTour />
+		<!-- <SectionTour /> -->
 	</ViewWrapper>
 </template>
