c583edac3e4da25edf92e6d8147187ef2f0de330
smaginweb <n.smagin@vk.team>
2024-02-13 12:24:08 +0300

    [FIX] Fix modal

---
+++ src/components/modal/modal-bonus/ModalBlockSuccess.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="modal-success-screen">
+		<h6 class="h-l">Спасибо за заявку!</h6>
+		<p class="text-sm">Администратор скоро с Вами свяжется!</p>
+	</div>
+</template>
+
+<style scoped>
+.modal-success-screen {
+	background-color: inherit;
+	border-radius: inherit;
+	position: absolute;
+	bottom: 0;
+	top: 0;
+	right: 0;
+	left: 0;
+	height: 100%;
+	width: 100%;
+
+	text-align: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 24px;
+
+	transition: 0.2s;
+	pointer-events: none;
+	z-index: 10;
+}
+</style>
