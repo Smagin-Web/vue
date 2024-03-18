c0e381cab16a6a5643124094aeabb5439233cae8
smaginweb <n.smagin@vk.team>
2024-03-19 00:41:15 +0300

    [FEATURE] ConceptPage - add sections

--- src/components/ConceptPage.vue
+++ src/components/ConceptPage.vue
@@ -9,6 +9,20 @@ import SectionPreview from '@/components/sections/shared/section-preview/Section
 import SectionHelp from '@/components/sections/shared/section-help/SectionHelp.vue'
 import SectionFaq from '@/components/sections/inside/section-faq/SectionFaq.vue'
 import SectionTech from '@/components/sections/inside/section-tech/SectionTech.vue'
+import SectionOffer from '@/components/sections/shared/section-offer/SectionOffer.vue'
+import SectionResult from '@/components/sections/shared/section-result/SectionResult.vue'
+import SectionProcedures from '@/components/sections/inside/section-procedures/SectionProcedures.vue'
+import SectionRec from '@/components/sections/inside/section-rec/SectionRec.vue'
+import SectionMasters from '@/components/sections/shared/section-masters/SectionMasters.vue'
+import SectionDevices from '@/components/sections/inside/section-devices/SectionDevices.vue'
+import SectionStories from '@/components/sections/shared/section-stories/SectionStories.vue'
+import SectionOther from '@/components/sections/shared/section-other/SectionOther.vue'
+import SectionTariff from '@/components/sections/inside/section-tariff/SectionTariff.vue'
+import SectionCircles from '@/components/sections/shared/section-circles/SectionCircles.vue'
+import SectionReview from '@/components/sections/shared/section-review/SectionReview.vue'
+import SectionPlaces from '@/components/sections/inside/section-places/SectionPlaces.vue'
+import SectionBonus from '@/components/sections/shared/section-bonus/SectionBonus.vue'
+import SectionRead from '@/components/sections/shared/section-read/SectionRead.vue'
 
 const props = defineProps(['conceptName'])
 </script>
@@ -36,7 +50,21 @@ const props = defineProps(['conceptName'])
 			:text4="conceptsInfo[props.conceptName].info2.text4"
 			:textDisable2="true"
 		/>
+		<SectionOffer />
+		<SectionResult />
+		<SectionProcedures />
+		<SectionRec />
+		<SectionMasters />
+		<SectionDevices />
+		<SectionStories />
+		<SectionOther />
+		<SectionTariff />
+		<SectionCircles />
+		<SectionReview />
 		<SectionHelp />
+		<SectionPlaces />
+		<SectionBonus />
+		<SectionRead />
 		<SectionFaq :items="conceptsInfo[props.conceptName].faq" />
 	</ViewWrapper>
 </template>
